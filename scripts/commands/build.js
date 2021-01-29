f08d030d592a22123b27f2dfe6a51d1a895a48a4
FAL <falworks.contact@gmail.com>
2021-01-30 04:38:01 +0900

    build: report elapsed time

--- scripts/commands/build.js
+++ scripts/commands/build.js
@@ -2,4 +2,4 @@ import * as build from "@fal-works/ts-build-helper/lib/build/index.js";
 import paths from "../paths.js";
 import config from "../config.js";
 
-void build.run(paths, config);
+export default build.run(paths, config);
